828e4714c9fc4bbe2ee810104cf6b53835e3bd36
brianmina <brian.mina17@gmail.com>
2025-02-17 06:39:15 -0500

    groups created inside class

--- main.py
+++ main.py
@@ -11,21 +11,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():         
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
             
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        player.update(dt)
+        
         
 if __name__ == "__main__":
     main() 
--- player.py
+++ player.py
@@ -3,9 +3,14 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
+    containers = None
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+        if Player.containers:
+            for group in Player.containers:
+                group.add(self)
 
 
     def triangle(self):
